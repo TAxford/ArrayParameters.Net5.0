4a545986b3737957ed0fa2c9b9bb19d22b8489af
Timothy Axford <t.axford2@yahoo.com>
2023-03-27 21:03:50 -0400

    Set up a double array for gradces with variables and a for loop to return average

--- ArrayParameters.Net5.0/Program.cs
+++ ArrayParameters.Net5.0/Program.cs
@@ -6,6 +6,29 @@ namespace ArrayParameters.Net5._0
     {
         static void Main(string[] args)
         {
+
+
+
+
+
+        }
+
+        static double GetAverage(int[] gradesArray)
+        {
+            int size = gradesArray.Length;
+            double average;
+            int sum = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                sum += gradesArray[i];
+            }
+            average = (double)sum / size;
+            return average;
+            
+
+
+
         }
     }
 }
